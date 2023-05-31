9ee25874b62136afabf6cb2c61f72f8bc3e3877f
Karin Hsu <khsu0720@gmail.com>
2023-05-30 21:45:54 -1000

    Initial build subtree commit

---
+++ precache-manifest.b2ff3681888e031188e3c5191d98ee6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e73f90be85122feb654884a7bf461fef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02d4c63f4ce36264eaa5",
+    "url": "/static/css/2.a82e94bd.chunk.css"
+  },
+  {
+    "revision": "42f0cdfa5147c7ec483b",
+    "url": "/static/css/main.4d365d65.chunk.css"
+  },
+  {
+    "revision": "02d4c63f4ce36264eaa5",
+    "url": "/static/js/2.3be133f4.chunk.js"
+  },
+  {
+    "revision": "42f0cdfa5147c7ec483b",
+    "url": "/static/js/main.97ddac7c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
